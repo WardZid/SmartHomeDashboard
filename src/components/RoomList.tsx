834ae549c14a778558f2bd4a52b77a8d95853bc3
Ward Zidani <86842722+WardZid@users.noreply.github.com>
2024-03-24 11:00:03 +0200

    Create RoomList.tsx

---
+++ src/components/RoomList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const RoomList: React.FC = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default RoomList;
