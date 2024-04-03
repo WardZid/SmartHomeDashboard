b283c5e2076a979ce3bdc2190aadc93460a86bb8
Ward Zidani <86842722+WardZid@users.noreply.github.com>
2024-04-03 16:01:50 +0300

    build

--- src/components/add_widget/AddWidgetDialog.tsx
+++ src/components/add_widget/AddWidgetDialog.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Dialog from '../Dialog';
 import * as deviceModel from "../../models/Device";
-import AddWidgetDeviceItem from "./AddWidgetItem";
+import AddWidgetDeviceItem from "./AddWidgetDeviceItem";
 
 interface AddWidgetDialogProps {
     isOpen: boolean;
