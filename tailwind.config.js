b8c4e30bb66b4049e91f76c773265af683b029ab
Ward Zidani <86842722+WardZid@users.noreply.github.com>
2024-04-21 01:05:57 +0300

    build

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,20 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/views/*.{js,jsx,ts,tsx}",
+    "./src/components/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        orange: '#F05622',
-        'dark-blue': '#042A35',
-        'light-blue': '#42B6E9',
-        'off-white': '#EEEEEE',
+        orange: "#F05622",
+        "dark-blue": "#042A35",
+        "light-blue": "#42B6E9",
+        "off-white": "#EEEEEE",
       },
     },
   },
   plugins: [],
   darkMode: "class",
-}
-
+};
